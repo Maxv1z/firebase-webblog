29a6787eca718d0f7efb9c29af4de03df2588ca0
Maxv1z <meitstudying@gmail.com>
2023-08-28 15:27:02 +0300

    Fixed bugs with time showing

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -1,5 +1,3 @@
-// CreatePost.js
-
 import React, { useState, useEffect } from 'react';
 import { addDoc, collection } from 'firebase/firestore';
 import { db, auth } from '../firebase-config';
@@ -8,6 +6,7 @@ import { useNavigate } from 'react-router-dom';
 const CreatePost = ({ isAuth }) => {
     const [title, setTitle] = useState('');
     const [postText, setPostText] = useState('');
+    const [creationDate, setCreationDate] = useState('');
 
     const postsCollectionRef = collection(db, 'posts');
 
@@ -15,28 +14,7 @@ const CreatePost = ({ isAuth }) => {
 
     const CreatePost = async () => {
         const currentUser = auth.currentUser;
-
-        function getRelativeTime(timestamp) {
-            const currentTime = new Date();
-            const postTime = new Date(timestamp);
-            const timeDifference = currentTime - postTime;
-
-            if (timeDifference < 60000) { // Less than a minute
-                return "just now";
-            } else if (timeDifference < 3600000) { // Less than an hour
-                const minutes = Math.floor(timeDifference / 60000);
-                return `${minutes} ${minutes === 1 ? 'minute' : 'minutes'} ago`;
-            } else if (timeDifference < 86400000) { // Less than a day
-                const hours = Math.floor(timeDifference / 3600000);
-                return `${hours} ${hours === 1 ? 'hour' : 'hours'} ago`;
-            } else if (timeDifference < 604800000) { // Less than a week
-                const days = Math.floor(timeDifference / 86400000);
-                return `${days} ${days === 1 ? 'day' : 'days'} ago`;
-            } else {
-                const options = { year: 'numeric', month: 'short', day: 'numeric' };
-                return postTime.toLocaleDateString(undefined, options);
-            }
-        }
+        const currentTime = new Date();
 
         await addDoc(postsCollectionRef, {
             title,
@@ -44,10 +22,13 @@ const CreatePost = ({ isAuth }) => {
             author: {
                 name: currentUser.displayName,
                 id: currentUser.uid,
-                avatar: currentUser.photoURL, // Store the avatar image URL
+                avatar: currentUser.photoURL,
             },
-            createdAt: getRelativeTime(new Date().toISOString()),
+            createdAt: currentTime,
         });
+
+        setCreationDate(currentTime);
+
         navigate('/');
     };
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,6 +13,29 @@ function Home({ isAuth }) {
         await deleteDoc(postDoc);
     };
 
+    function getRelativeTime(createdAt) {
+        const postTime = new Date(createdAt);
+        console.log(postTime, "asdfasdfasdf")
+        const currentTime = new Date();
+        const timeDifference = currentTime - postTime;
+
+        if (timeDifference < 60000) { // Less than a minute
+            return "just now";
+        } else if (timeDifference < 3600000) { // Less than an hour
+            const minutes = Math.floor(timeDifference / 60000);
+            return `${minutes} ${minutes === 1 ? 'minute' : 'minutes'} ago`;
+        } else if (timeDifference < 86400000) { // Less than a day
+            const hours = Math.floor(timeDifference / 3600000);
+            return `${hours} ${hours === 1 ? 'hour' : 'hours'} ago`;
+        } else if (timeDifference < 604800000) { // Less than a week
+            const days = Math.floor(timeDifference / 86400000);
+            return `${days} ${days === 1 ? 'day' : 'days'} ago`;
+        } else {
+            const options = { year: 'numeric', month: 'short', day: 'numeric' };
+            return postTime.toLocaleDateString(undefined, options);
+        }
+    }
+
     useEffect(() => {
         const unsubscribe = onSnapshot(postsCollectionRef, (snapshot) => {
             const updatedPosts = snapshot.docs.map((doc) => ({
@@ -27,6 +50,7 @@ function Home({ isAuth }) {
     return (
         <div className="homePage">
             {postLists.map((post) => {
+                const createdAtDate = new Date(post.createdAt.seconds * 1000)
                 return (
                     <div className="post" key={post.id}>
                         <div className="postHeader">
@@ -49,7 +73,8 @@ function Home({ isAuth }) {
                         <div className="postTextContainer">{post.postText}</div>
                         <div className="time-and-author">
                             <h3>@{post.author.name}</h3>
-                            <h3 className="time">{post.createdAt}</h3>
+                            <h3 className="time">{getRelativeTime(createdAtDate)}</h3>
+
                         </div>
                     </div>
                 );
